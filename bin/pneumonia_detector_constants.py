28bff5fe6e7f5b3b7b6d463986e2ccf4f249e30c
Ravi Teja Kothuru <rkothuru@sandiego.edu>
2024-08-07 06:23:30 +0530

    Trained models for First and Second Order features separately

--- bin/pneumonia_detector_constants.py
+++ bin/pneumonia_detector_constants.py
@@ -2,6 +2,7 @@ pneumonia_detector_constants = {
 	"chest_xray_folder_name": "chest_xray",
 	"dataset_dir_name": "dataset",
 	"image_information_dir_name": "image_information",
+	"trained_model_pkl_files_dir_name": "trained_models",
 	"image_info_xls_file_name": "chest_xray_images_info.xlsx",
 	"image_first_order_features_xls_file_name": "chest_xray_images_first_order_features.xlsx",
 	"image_second_order_features_glcm_xls_file_name": "chest_xray_images_second_order_features_glcm.xlsx",
@@ -17,5 +18,6 @@ pneumonia_detector_constants = {
 	"excel_sheet_names": [
 			'Train Normal Features', 'Train Pneumonia Features',
 			'Test Normal Features', 'Test Pneumonia Features'
-		]
+		],
+	"target_column_name": "Pneumonia"
 }
